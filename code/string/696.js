66e8c282303b94f62daad147dd283f712f851be3
young <iezzuyoung@gmail.com>
2019-03-01 14:04:33 +0800

    feat: 696

---
+++ code/string/696.js
@@ -0,0 +1,17 @@
+export default (str) => {
+  let r = []
+
+  for (let i = 0, len = str.length - 1; i < len; i++) {
+    let sub = match(str.slice(i))
+    let match = (str) => {
+      let j = str.match(/^(0+|1+)/)[0]
+      let o = (j[0] ^ 1).toString().repeat(j.length)
+      let reg = new RegExp(`^(${j}${o})`)
+    }
+    if (sub) {
+      r.push(sub)
+    }
+  }
+
+  return ''
+}
