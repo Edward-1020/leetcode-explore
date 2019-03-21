8321ae50d0b27dd6a40b982b285cd0f55769aae2
young <yangxu@yeezon.com>
2019-03-21 22:49:11 +0800

    feat: oddSort

--- test/sort/2.test.js
+++ test/sort/2.test.js
@@ -1,4 +1,4 @@
-import sort from '../../code/sort/1'
+import sort from '../../code/sort/2'
 
 test('选择排序', () => {
   expect(sort([1, 9, 5, 3, 4])).toEqual([1, 3, 4, 5, 9])
---
+++ test/sort/922.test.js
@@ -0,0 +1,5 @@
+import sort from '../../code/sort/922.js'
+
+test('oddSort', () => {
+    expect(sort([4, 2, 5, 7])).toEqual([2,5,4,7])
+})
