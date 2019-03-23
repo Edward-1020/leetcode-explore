fa31d4ec1eb2477d3e38c1691c05ad3051600e94
young <iezzuyoung@gmail.com>
2019-03-23 08:38:42 +0800

    feat: 215

--- code/sort/215.js
+++ code/sort/215.js
@@ -1,3 +1,17 @@
 // export default (arr, k) => {
 //   return arr.sort((a, b) => b - a)[k - 1]
 // }
+
+export default (arr, k) => {
+  let len = arr.length - 1
+  for (let i = len; i > len - k; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let tmp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = tmp
+      }
+    }
+  }
+  return arr[len - k + 1]
+}
---
+++ test/sort/215.test.js
@@ -0,0 +1,5 @@
+import sort from '../../code/sort/215.js'
+
+test('sort:input 2', () => {
+  expect(sort([3, 2, 1, 5, 6, 2])).toEqual([5])
+})
