a92f49ded73052fd74ab6374acc7040c34b9250a
Edward-1020 <iezzuyoung@gmail.com>
2019-05-17 15:07:11 +0800

    feat: improve

--- code/array/914.js
+++ code/array/914.js
@@ -1,6 +1,16 @@
 export default (arr) => {
-  let str = arr.sort().join('')
-  let group = str.match(/(\d)\1+|\d/g)
+  // 存储每张卡牌的总数
+  // 修改排序的方式修改为直接统计每个相同字符的数量，思路不变（LeetCode测试用例）
+  let group = []
+  let tmp = {}
+  arr.forEach(item => {
+    tmp[item] = tmp[item] ? tmp[item] + 1 : 1
+  })
+  for (let v of Object.values(tmp)) {
+    group.push(v)
+  }
+  // 此时group已经存放的是每张牌的总数了（数组只遍历一遍，避免了排序和正则的耗时）
+  // 求两个数的最大公约数
   let gcd = (a, b) => {
     if (b === 0) {
       return a
@@ -9,14 +19,14 @@ export default (arr) => {
     }
   }
   while (group.length > 1) {
-    let a = group.shift().length
-    let b = group.shift().length
+    let a = group.shift()
+    let b = group.shift()
     let v = gcd(a, b)
     if (v === 1) {
       return false
     } else {
-      group.unshift('0'.repeat(b))
+      group.unshift(v)
     }
   }
-  return group.length ? group[0].length > 1 : false
+  return group.length ? group[0] > 1 : false
 }
